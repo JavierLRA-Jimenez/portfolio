22d41da24ec5e8d928999559e0f98262ab9fa7ab
JavierLRA-Jimenez <javierjimenezmk@gmail.com>
2023-06-29 17:02:43 -0400

    js cambios

--- src/data/Portfoliojs.js
+++ src/data/Portfoliojs.js
@@ -1,19 +1,19 @@
 export default [
     {
         title:'Hero Portfolio / 3D and all Css skills',
-        imgUrl: 'public/assets/OldPortfolio.png',
+        imgUrl: 'assets/OldPortfolio.png',
         stack: ['Html', 'Css'],
         link:'https://javierlra-jimenez.github.io/',
     },
     {
         title:'Restaurant Finder/ practice',
-        imgUrl: 'public/assets/Restaurant.png',
+        imgUrl: 'assets/Restaurant.png',
         stack: ['React-Js', 'Tailwind', 'Vite'],
         link:'https://javierlra-jimenez.github.io/Restaurant-/',
     },
     {
         title:'Artist Landing Page / Actual Client',
-        imgUrl: 'public/assets/Actualclient.png',
+        imgUrl: 'assets/Actualclient.png',
         stack: ['React-Js', 'Tailwind', 'Vite'],
         link:'https://lezflp.netlify.app/',
     },
