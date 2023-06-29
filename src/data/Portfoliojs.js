79b82edb3112cc109fe7a38d991c2d32b181e424
JavierLRA-Jimenez <javierjimenezmk@gmail.com>
2023-06-29 16:57:00 -0400

    Mover y renombrar archivos en la carpeta de activos

--- src/data/Portfoliojs.js
+++ src/data/Portfoliojs.js
@@ -1,19 +1,19 @@
 export default [
     {
         title:'Hero Portfolio / 3D and all Css skills',
-        imgUrl: 'src/assets/OldPortfolio.png',
+        imgUrl: 'public/assets/OldPortfolio.png',
         stack: ['Html', 'Css'],
         link:'https://javierlra-jimenez.github.io/',
     },
     {
         title:'Restaurant Finder/ practice',
-        imgUrl: 'src/assets/Restaurant.png',
+        imgUrl: 'public/assets/Restaurant.png',
         stack: ['React-Js', 'Tailwind', 'Vite'],
         link:'https://javierlra-jimenez.github.io/Restaurant-/',
     },
     {
         title:'Artist Landing Page / Actual Client',
-        imgUrl: 'src/assets/Actualclient.png',
+        imgUrl: 'public/assets/Actualclient.png',
         stack: ['React-Js', 'Tailwind', 'Vite'],
         link:'https://lezflp.netlify.app/',
     },
